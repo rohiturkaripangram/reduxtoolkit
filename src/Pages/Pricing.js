6b837260533cdfc48dbe70b0402bafd0a9af745d
Rohitturkar <146061501+rohiturkaripangram@users.noreply.github.com>
2023-10-16 11:15:20 +0530

    Update Pricing.js

--- src/Pages/Pricing.js
+++ src/Pages/Pricing.js
@@ -1,12 +1,29 @@
 import {publishableKey} from "../index"
 import {PricingKey} from "../index"
+
+import { useAuth0 } from "@auth0/auth0-react";
+
 const PricingPage=()=> {
   
-  
+  const { isAuthenticated, loginWithRedirect} = useAuth0();
   return (
-    <stripe-pricing-table pricing-table-id={`${PricingKey}`}
+    isAuthenticated?(
+<stripe-pricing-table pricing-table-id={`${PricingKey}`}
     publishable-key={`${publishableKey}`}>
     </stripe-pricing-table>
+    )
+    
+    : <div className="flex justify-center items-center flex-col gap-y-4 w-screen h-96">
+    <h1>Please Login to continue</h1>
+    <button
+      type="button"
+      class="rounded-md bg-button-bg px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-button-bg focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+      onClick={() => loginWithRedirect()}
+    >
+      Login
+    </button>
+  </div>
+
   );
 }
 
